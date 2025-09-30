9d0871ef077cb55eecfbe19d84d631bf9c3676d7
DaromeGal <darome325@gmail.com>
2025-09-30 13:42:55 +0200

    Ejercicio resuelto

---
+++ src/main/java/org/example/ClientChat.java
@@ -0,0 +1,48 @@
+package org.example;
+ import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+    public class ClientChat {
+        public static void main(String[] args) {
+
+            String host = "localhost"; // pepe ==> 10.101.3.86
+
+
+            int puerto = 6000; // puerto remoto
+            Scanner sc = new Scanner(System.in);
+            // El server esta en ip 10.101.21.152
+            // ABRIR SOCKET
+            try {
+                Socket cliente = new Socket(host, puerto); // conecta
+//            InetAddress inet = cliente.getInetAddress();
+//            System.out.println("IP local usada por el cliente: " +inet.toString());
+//            System.out.println("Puerto local: " + cliente.getLocalPort());
+
+                System.out.println("Datos de conexion externa");
+                System.out.println("Puerto remoto: " + cliente.getPort());
+                System.out.println("Host remoto: " + cliente.getInetAddress().getHostName());
+                System.out.println("IP Host remoto: " + cliente.getInetAddress().toString());
+
+                while(true) {
+                    // VOY A ESCRIBIR ALGO
+                    DataOutputStream out = new DataOutputStream(cliente.getOutputStream());
+                    System.out.println("Escribe algo chatin");
+                    String envio = sc.nextLine();
+                    out.writeUTF(envio);
+
+                    //respuesta
+                    DataInputStream in = new DataInputStream(cliente.getInputStream());
+                    String msg = in.readUTF();
+                    System.out.println("Mensaje recibido: "+ msg);
+                    System.out.println("=================================");
+                }
+                //cliente.close(); // cierra el socket
+            } catch (IOException e) {
+                System.err.println("Error de conexion: " + e.getMessage());
+            }
+
+        }
+}
---
+++ src/main/java/org/example/ServerChat.java
@@ -0,0 +1,51 @@
+package org.example;
+
+
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerChat {
+
+    public static void main(String[] args) throws IOException {
+
+        int puerto = 6000; // Puerto
+        Scanner sc = new Scanner(System.in);
+
+        ServerSocket servidor = new ServerSocket(puerto);
+        System.out.println("Escuchando en " + servidor.getLocalPort());
+
+        // esperando a un cliente
+        Socket cliente1 = servidor.accept();
+        System.out.println("Se ha conectado el cliente.");
+
+        // Crear streams para comunicarse con el cliente
+        DataInputStream in = new DataInputStream(cliente1.getInputStream());
+        DataOutputStream out = new DataOutputStream(cliente1.getOutputStream());
+
+        try {
+            while (true) {
+                // Recibir mensaje del cliente
+                String recibido = in.readUTF();
+                System.out.println("Cliente: " + recibido);
+
+                // Enviar respuesta al cliente
+                System.out.print("Servidor: ");
+                String respuesta = sc.nextLine();
+                out.writeUTF(respuesta);
+            }
+        } catch (IOException e) {
+            System.out.println("Finalizando el programa...");
+        } finally {
+            // Cerrar recursos
+            in.close();
+            out.close();
+            cliente1.close();
+            servidor.close();
+        }
+    }
+}
